4e49f0fd69948c928173626329b50c6e2f57762d
UnDer-7 <mateus7532@gmai.com>
2019-03-19 17:20:06 -0300

    tst de component

---
+++ src/app/shared/bus-list/bus-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bus-list',
+  templateUrl: './bus-list.component.html',
+  styleUrls: ['./bus-list.component.scss'],
+})
+export class BusListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
