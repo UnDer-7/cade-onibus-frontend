2c6b45c2a88678802ff839a492747142380ace7d
UnDer-7 <mateus7532@gmail.com>
2019-03-18 21:15:32 -0300

    Side menu

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(
+    private menuCtrl: MenuController
+  ) { }
 
+  public openFrist(): void {
+    this.menuCtrl.enable(true, 'main');
+    this.menuCtrl.open('main');
+  }
 }
