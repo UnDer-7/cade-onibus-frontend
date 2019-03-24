3b8c4f66ffc984f590e0163891aff65d24fb2d45
Lucas Veiga <lucasveiga09@gmail.com>
2019-03-24 17:47:31 -0300

    Adicionando listagem dos onibus do usuario na home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
-import { MenuController } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
+import { TokenService } from 'src/app/auth/Token.service';
+import { User } from 'src/app/models/user.model';
+import { UtilService } from 'src/app/util/util.service';
+import { HomeService } from './home.service';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +12,28 @@ import { Router } from '@angular/router';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  private token: any;
+  private user: User;
+
   constructor(
     private menuCtrl: MenuController,
-    private router: Router
+    private tokenService: TokenService,
+    private homeService: HomeService,
+    private util: UtilService,
+    private router: Router,
   ) { }
+
+  ionViewDidEnter() {
+    this.token = this.tokenService.decodeToken();
+    this.homeService.findUser(this.token._id).subscribe(
+        res => this.user = res,
+        err => {
+          this.util.showToast('Houve um erro na requisição. Tente recarregar a página.');
+          console.error(err);
+        }
+    );
+  }
 
   public openFrist(): void {
     this.menuCtrl.enable(true, 'main');
---
+++ src/app/pages/home/home.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../../models/user.model';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class HomeService {
+  private resourceUrl: string = environment.apiUrl + '/users';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public findUser(id: string): Observable<User> {
+    return this.http.get<User>(`${this.resourceUrl}/${id}`);
+  }
+}
