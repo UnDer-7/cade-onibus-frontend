45af0679784986af0c029c2d31fc872e1e279b78
Lucas Veiga <lucasveiga09@gmail.com>
2019-04-22 19:43:04 -0300

    pegando dados do usuario

--- src/app/pages/loja/loja.component.ts
+++ src/app/pages/loja/loja.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../modals/user-form/user.service';
+import { User } from 'src/app/models/user.model';
+import { TokenService } from 'src/app/auth/token.service';
+import { ToastController } from '@ionic/angular';
+import { UtilService } from 'src/app/util/util.service';
 
 @Component({
   selector: 'app-loja',
@@ -7,8 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LojaComponent implements OnInit {
 
-  constructor() { }
+  user: User;
 
-  ngOnInit() {}
+  constructor(
+    private userService: UserService,
+    private tokenService: TokenService,
+    private util: UtilService
+  ) { }
+
+  ngOnInit() {
+    const id = this.tokenService.decodeToken()._id;
+    this.userService.findUser(id).subscribe(
+      user => this.user = user,
+      () => this.util.showToast('Houve um erro ao carregar os dados!', 'danger')
+    )
+  }
 
 }
