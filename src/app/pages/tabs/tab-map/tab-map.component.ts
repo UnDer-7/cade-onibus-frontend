acd3ced5c1aa3152cd9be6459e3864dce4280f8a
UnDer-7 <mateus7532@gmail.com>
2019-05-07 00:40:24 -0300

    fix

--- src/app/pages/tabs/tab-map/tab-map.component.ts
+++ src/app/pages/tabs/tab-map/tab-map.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-tab-map',
   templateUrl: 'tab-map.component.html',
 })
-export class TabMapComponent {}
+export class TabMapComponent {
+  public readonly appName: string = environment.appName;
+  public readonly appColor: string = environment.contentColor;
+}
