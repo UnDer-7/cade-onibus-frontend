23de32df5dff0310e3bd3fad8b513d5664cb0787
UnDer-7 <mateus7532@gmail.com>
2019-05-09 01:04:12 -0300

    perfil made

--- src/app/pages/tabs/tab-perfil/tab-perfil-component.ts
+++ src/app/pages/tabs/tab-perfil/tab-perfil-component.ts
@@ -1,7 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { finalize } from 'rxjs/operators';
+import { environment } from '../../../../environments/environment';
+import { decodeJWT } from '../../../auth/jwt.handler';
+import { User } from '../../../models/user.model';
+import { UserService } from '../../../resource/user.service';
 
 @Component({
   selector: 'app-tab-perfil',
   templateUrl: 'tab-perfil-component.html',
 })
-export class TabPerfilComponent {}
+export class TabPerfilComponent implements OnInit {
+  public readonly appName: string = environment.appName;
+  public readonly appColor: string = environment.contentColor;
+  public isLoading: boolean = false;
+
+  public user: User = {} as User;
+
+  constructor(
+    private userService: UserService,
+  ) {
+  }
+
+  public ngOnInit(): void {
+    this.getUser();
+  }
+
+  public canShowLine(last: boolean): string | undefined {
+    if (last) return 'none';
+  }
+
+  private getUser(): void {
+    this.isLoading = true;
+    setTimeout(() => {
+      this.userService.getUser(decodeJWT().email).pipe(
+        finalize(() => this.isLoading = false),
+      ).subscribe(res => this.user = res);
+    }, 3000);
+  }
+}
--- src/app/pages/tabs/tab-perfil/tab-perfil.module.ts
+++ src/app/pages/tabs/tab-perfil/tab-perfil.module.ts
@@ -1,5 +1,6 @@
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { UserService } from '../../../resource/user.service';
 import { COMMON_IMPORTS } from '../../../utils/common-imports';
 import { TabPerfilComponent } from './tab-perfil-component';
 
@@ -8,6 +9,7 @@ import { TabPerfilComponent } from './tab-perfil-component';
     COMMON_IMPORTS,
     RouterModule.forChild([{ path: '', component: TabPerfilComponent }]),
   ],
+  providers: [UserService],
   declarations: [TabPerfilComponent],
 })
 export class TabPerfilModule {}
