5c6c79cb7bf4df170376b3d15d5c0f1239cb054d
UnDer-7 <mateus7532@gmail.com>
2019-05-10 21:51:51 -0300

    sessionstorage to localStorage

--- src/app/auth/jwt.handler.ts
+++ src/app/auth/jwt.handler.ts
@@ -2,15 +2,15 @@ import * as jwtDecode from 'jwt-decode';
 import { Token } from '../models/token.model';
 
 export function saveJWT(jwt: string): void {
-  sessionStorage.setItem('token', jwt);
+  localStorage.setItem('token', jwt);
 }
 
 export function getJWT(): string | null {
-  return sessionStorage.getItem('token');
+  return localStorage.getItem('token');
 }
 
 export function removeJWT(): void {
-  sessionStorage.removeItem('token');
+  localStorage.removeItem('token');
 }
 
 export function decodeJWT(): Token {
