13cef3e9d1472765676cdaccfad005bc7f96ea6e
UnDer-7 <mateus7532@gmail.com>
2020-02-25 18:22:24 -0300

    deleting project

--- src/app/model/token.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Token {
-  email: string;
-  createdAt: string;
-  iat: number;
-  exp: number;
-}
--- src/app/utils/components-utils.ts
+++
@@ -1,6 +0,0 @@
-import { environment } from '../../environments/environment';
-
-export abstract class ComponentsUtils {
-  public readonly appColor: string = environment.contentColor;
-  public readonly appName: string = environment.appName;
-}
