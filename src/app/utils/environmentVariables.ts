3c3dd740d8feefaae57b0e99655bebaf097d9425
UnDer-7 <mateus7532@gmail.com>
2020-04-02 11:11:13 -0300

    added Environment Variables

--- src/app/utils/environmentVariables.ts
+++ src/app/utils/environmentVariables.ts
@@ -3,8 +3,13 @@ export default class EnvVariables {
 
   public static readonly PRIMARY_COLOR: string = '#7044ff';
 
-  // @ts-ignore
-  public static readonly BASE_URL: string = 'http://localhost:8080';
+  public static readonly BASE_URL: string = EnvVariables.getVariable('REACT_APP_BASE_URL');
 
-  // public static readonly DF_TRANS_BASE_URL: string | undefined = process.env.DF_TRANS_BASE_URL;
+  public static readonly DF_TRANS_BASE_URL: string = EnvVariables.getVariable('REACT_APP_DF_TRANS_BASE_URL');
+
+  private static getVariable(name: string): string {
+    const env = process.env[name];
+    if (!env) throw new Error(`Environment Variable [${name}] Not Fount`);
+    return env;
+  }
 }
